4da7b6b0001f1a7ed93e6b522cc0cb3d3648987b
Sudharaka Ashen <143466124+SudharakaA@users.noreply.github.com>
2024-03-15 21:23:05 +0530

    Add files via upload

---
+++ customermanagement1demo 2/src/main/java/com/example/customermanagement1demo/Customermanagement1demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.customermanagement1demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Customermanagement1demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Customermanagement1demoApplication.class, args);
+	}
+
+}
---
+++ customermanagement1demo 2/src/test/java/com/example/customermanagement1demo/Customermanagement1demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customermanagement1demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Customermanagement1demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
